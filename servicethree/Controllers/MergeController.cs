84ec998b8e4bec1eba0e259f11ef9914ebf6bab2
Edwin Emery <edwin.emery@avanade.com>
2021-06-17 10:43:58 +0100

    Project creation

---
+++ servicethree/Controllers/MergeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace servicethree.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class MergeController : ControllerBase
+    {
+        private IConfiguration Configuration;
+        public MergeController(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            var numbersService = $"{Configuration["numbersServiceURL"]}/numbers";
+            var serviceOneResponseCall = await new HttpClient().GetStringAsync(numbersService);
+            var lettersService = $"{Configuration["lettersServiceURL"]}/letters";
+            var serviceTwoResponseCall = await new HttpClient().GetStringAsync(lettersService);
+            var mergedResponse = $"{serviceOneResponseCall}{serviceTwoResponseCall}";
+            return Ok(mergedResponse);
+        }
+    }
+}
